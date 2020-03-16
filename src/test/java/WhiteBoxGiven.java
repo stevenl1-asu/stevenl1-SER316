0c2d307d5a19923762943868e1023d48ea6271a5
Steven Le <stevenl1@asu.edu>
2020-03-16 15:15:00 -0700

    Completed Task 1

--- src/test/java/WhiteBoxGiven.java
+++ src/test/java/WhiteBoxGiven.java
@@ -1,11 +1,14 @@
 package test.java;
 
 import main.java.Cart;
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+
 
 public class WhiteBoxGiven { 
 
